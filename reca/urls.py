65250bdbf76f277d541949a350057ec07fca384b
kh.lee@corein.co.kr <kh.lee@corein.co.kr>
2019-04-26 19:44:12 +0900

    2019-04-26 19:43

--- reca/urls.py
+++ reca/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('home', views.index, name='index'),
+    path('rp_period_user', views.rp_period_user, name='rp_period_user'),
     path('ls_period_cs', views.ls_period_cs, name='ls_period_cs'),
+    path('ls_period_csct', views.ls_period_csct, name='ls_period_csct'),
     path('card_pm', views.card_pm, name='card_pm'),
     path('dash', views.dash, name='dash'),
 ]
